bfca8802fb5905e5f5777189e376ef4ea0b7ca46
ahmad abu labeb <ahmadlabeb1992@gmail.com>
2017-02-28 19:14:22 +0200

    create first project django

---
+++ ahmad/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class album(models.Model):
+    artist=models.CharField(max_length=250)
+    albume_title=models.CharField(max_length=250)
+    genor=models.CharField(max_length=250)
+    albume_logo=models.CharField(max_length=1000)
+
+class song(models.Model):
+    albums=models.ForeignKey(album,on_delete=models.CASCADE)
+    fileType=models.CharField(max_length=250)
+    song_title=models.CharField(max_length=200)
---
+++ ahmad/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(r):
+    return HttpResponse("ahmad abu labeb")
+def about(r):
+    return HttpResponse("<h1>abu labeb</h1>")
